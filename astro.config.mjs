d74903c676114aa2f7cdf4c5a3be7e09250e985f
kaizenics <nicosejohnsoriano@gmail.com>
2024-08-10 01:04:55 +0800

    refactor: update navigation menu labels and structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,15 +11,27 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Getting Started',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Example Guide', slug: 'getting-started/guide' },
 					],
 				},
 				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
+				},
+				{
+					label: 'Contributing',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Forking a Repository', slug: 'contributing/forking' },
+						{ label: 'Cloning the Forked Repository', slug: 'contributing/cloning' },
+						{ label: 'Creating New Branch', slug: 'contributing/new-branch' },
+						{ label: 'Making Changes', slug: 'contributing/making-changes' },
+						{ label: 'Committing and Pushing Changes', slug: 'contributing/commit' },
+						{ label: 'Creating a Pull Request', slug: 'contributing/pull-request' },
+					],
 				},
 			],
 		}),
